318f89d34b56019f699f5ce40efb12b2aead79bf
Frank Aigner <frank_aigner@hotmail.com>
2023-03-30 23:07:58 +0200

    config load

---
+++ config/config.go
@@ -0,0 +1,92 @@
+package config
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"os"
+)
+
+type Config struct {
+	MySQL       MySQLConfig       `json:"mysql"`
+	API         APIConfig         `json:"api"`
+	LetsEncrypt LetsEncryptConfig `json:"letsencrypt"`
+}
+
+type MySQLConfig struct {
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Database string `json:"database"`
+}
+
+type APIConfig struct {
+	APIKey         string          `json:"api_key"`
+	MessageTargets []MessageTarget `json:"MessageTargets"`
+}
+
+type MessageTarget struct {
+	Name    string `json:"name"`
+	Type    string `json:"type"`
+	Webhook string `json:"webhook"`
+}
+
+type LetsEncryptConfig struct {
+	Email   string   `json:"email"`
+	Domains []string `json:"domains"`
+}
+
+func LoadConfig() (*Config, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	configFile, err := os.Open(dir + "/config/config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer configFile.Close()
+
+	var config Config
+	err = json.NewDecoder(configFile).Decode(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	err = validateConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func validateConfig(config Config) error {
+
+	if len(config.API.MessageTargets) == 0 {
+		return errors.New("invalid config: missing message targets")
+	}
+	if config.MySQL.Host == "" {
+		return errors.New("invalid config: missing DB server")
+	}
+	if config.MySQL.Port == 0 {
+		return errors.New("invalid config: missing DB port")
+	}
+	if config.MySQL.Username == "" {
+		return errors.New("invalid config: missing DB user")
+	}
+	if config.MySQL.Password == "" {
+		return errors.New("invalid config: missing DB password")
+	}
+	if config.API.APIKey == "" {
+		return errors.New("invalid config: missing API key")
+	}
+	if config.LetsEncrypt.Email == "" {
+		return errors.New("invalid config: missing email for Let's Encrypt")
+	}
+	if len(config.LetsEncrypt.Domains) == 0 {
+		return errors.New("invalid config: missing domains for Let's Encrypt")
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/FrAigner/Message-Broker/apiEndpoints"
 )
@@ -16,34 +14,11 @@ func main() {
 	// API-Endpunkte definieren
 	http.HandleFunc("/api/nachrichten", apiEndpoints.HandleNachrichten)
 	http.HandleFunc("/health", apiEndpoints.HandleHealthCheck)
+
 	// Server starten
 	log.Println("Server gestartet")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("Server konnte nicht gestartet werden: ", err)
 	}
-}
-
-type Config struct {
-	MessageTargets map[string]string `json:"messageTargets"`
-	DBServer       string            `json:"dbServer"`
-	DBPort         int               `json:"dbPort"`
-	DBUser         string            `json:"dbUser"`
-	DBPassword     string            `json:"dbPassword"`
-}
-
-func LoadConfig() (*Config, error) {
-	configFile, err := os.Open("config.json")
-	if err != nil {
-		return nil, err
-	}
-	defer configFile.Close()
-
-	var config Config
-	err = json.NewDecoder(configFile).Decode(&config)
-	if err != nil {
-		return nil, err
-	}
-
-	return &config, nil
 }
